dc7b986682b9efc0fc20a578fbe04f400754d84b
Fernando-Filho <fernando.fs.filhorn@gmail.com>
2022-07-06 23:39:18 -0300

    algumas alterações, desejo fazer um HTML e CSS do zero para poder fazer um desejo de um relógio, ao final linkar ao JS

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ window.onload = function () {
     var hours = 00;
     var minutes = 00;
     var seconds = 00;
-    var tens = 00;
-    var appendTens = document.getElementById("tens")
+    var milissegundos = 00;
+    var appendmilissegundos = document.getElementById("milissegundos")
     var appendSeconds = document.getElementById("seconds")
     var appendMinutos = document.getElementById("minutos")
     var appendHoras = document.getElementById("horas")
-    var buttonStart = document.getElementById('button-start');
-    var buttonStop = document.getElementById('button-stop');
-    var buttonReset = document.getElementById('button-reset');
+    var buttonStart = document.getElementById('button-start')
+    var buttonStop = document.getElementById('button-stop')
+    var buttonReset = document.getElementById('button-reset')
     var Interval;
 
     buttonStart.onclick = function () {
@@ -25,34 +25,34 @@ window.onload = function () {
 
     buttonReset.onclick = function () {
         clearInterval(Interval);
-        tens = "00";
+        milissegundos = "00";
         seconds = "00";
         minutes = "00";
         hours = "00";
-        appendTens.innerHTML = tens;
+        appendmilissegundos.innerHTML = milissegundos;
         appendSeconds.innerHTML = seconds;
     }
 
 
 
     function startTimer() {
-        tens++;
+        milissegundos++;
 
-        if (tens <= 9) {
-            appendTens.innerHTML = "0" + tens;
+        if (milissegundos <= 9) {
+            appendmilissegundos.innerHTML = "0" + milissegundos;
         }
 
-        if (tens > 9) {
-            appendTens.innerHTML = tens;
+        if (milissegundos > 9) {
+            appendmilissegundos.innerHTML = milissegundos;
 
         }
 
-        if (tens > 99) {
+        if (milissegundos > 99) {
             console.log("seconds");
             seconds++;
             appendSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            appendTens.innerHTML = "0" + 0;
+            milissegundos = 0;
+            appendmilissegundos.innerHTML = "0" + milissegundos;
         }
 
         if (seconds > 9) {
@@ -64,7 +64,7 @@ window.onload = function () {
             minutes++;
             appendMinutos.innerHTML = "0" + minutes;
             seconds = 0;
-            appendSeconds.innerHTML = "0" + 0; 
+            appendSeconds.innerHTML = "0" + seconds; 
         }
 
         if (minutes > 9) {
@@ -76,7 +76,7 @@ window.onload = function () {
             hours++;
             appendSeconds.innerHTML = "0" + hours;
             minutes = 0;
-            appendMinutos.innerHTML = "0" + 0; 
+            appendMinutos.innerHTML = "0" + minutes; 
         }
 
         if (minutes > 9) {
